79197d39984aabfbd7e1cb1991ac8d260cb9a7dd
ompujari9512 <166282650+ompujari9512@users.noreply.github.com>
2026-02-12 16:58:27 +0530

    Auto-create superuser

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py migrate
+
+# Create superuser automatically (fails silently if user already exists)
+python manage.py createsuperuser --noinput || true
